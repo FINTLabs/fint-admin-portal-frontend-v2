73d5f4f7eb343a4edc419af2598606a48942afb5
jennifer-fintlabs <jennifer@fintlabs.no>
2025-05-19 10:32:41 +0200

    adding cypress tests to deploy 🐰

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,26 @@
+// ***********************************************************
+// This example support/e2e.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands';
+
+// Set up MSW for server-side requests
+if (Cypress.env('VITE_MOCK_CYPRESS')) {
+    // This will be available to the Node.js environment
+    process.env.VITE_MOCK_CYPRESS = 'true';
+}
+
+// Alternatively, you can expose commands to Cypress namespace
+// Cypress.Commands.add('login', (email, password) => { ... })
